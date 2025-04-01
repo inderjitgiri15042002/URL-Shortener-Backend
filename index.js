bc8f9576f3d97d687894e7224ce9fc763037c196
Inderjit Giri <inderjitgiri2002@gmail.com>
2025-04-01 11:33:23 +0530

    Added authentication middleware and route access control

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require("express");
-const { connectToMongoDB } = require("./connect");
-const path = require("path");
-const urlRoute = require("./routes/url");
 const app = express();
+const path = require("path");
 const PORT = 8001;
+const cookieParser = require("cookie-parser");
+
+const { restrictToLoggedinUserOnly, checkAuth } = require("./middlewares/auth");
+
+const { connectToMongoDB } = require("./connect");
 const URL = require("./models/url");
+
+const userRouter = require("./routes/user");
+const urlRoute = require("./routes/url");
 const staticRoute = require("./routes/staticRouter");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 // set a view Engine to ejs
 // use res.render to load up an ejs view file
@@ -16,8 +23,9 @@ app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
-app.use("/url", urlRoute);
-app.use("/", staticRoute);
+app.use("/", checkAuth, staticRoute);
+app.use("/url", restrictToLoggedinUserOnly, urlRoute);
+app.use("/user", userRouter);
 
 app.get("/test", async (req, res) => {
   const allUrls = await URL.find({});
